533527abae3bf31baf904aaa082ddf3ad5473b88
zhaorong <zhaorong@kuaishou.com>
2021-10-12 17:18:29 +0800

    feat: add file

---
+++ 13.tricks/13.lodash.set.js
@@ -0,0 +1,33 @@
+function set(target, path, value) {
+  const paths = `${path}`.match(/(\w|\$)+/g)
+  if (paths && paths.length) {
+    const lastKey = paths.pop();
+    const last = get(target, paths.join('.'), {})
+    last[lastKey] = value
+  }
+  return target;
+}
+function set(obj, path, value) {
+  path = Array.isArray(path) ? path :  path.replace('[', '.').replace(']', '').split('.');
+  src = obj;
+  path.forEach((key, index, array) => {
+    if (index == path.length - 1) {
+      src[key] = value;
+    } else {
+      if (!src.hasOwnProperty(key)) { // if the key doesn't exist on object
+        const next = array[index + 1];
+        src[key] = String(Number(next)) === next ? [] : {}; // create a new object if next is item in array is not a number
+      }
+      src = src[key];
+    }
+  })
+}
+
+let user = { };​
+// set(user, "images[0].url", "xxxx");​
+console.log(user);
+// set(user, "images[0].width", 300);​
+// 调用后user的内容为​
+// {​
+//    images: [{url: "xxx", width: 300}]​
+// }
